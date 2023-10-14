6422bf32af433038d48771af4f6ff3542eddbf06
carlosedu1234 <65097149+carlosedu1234@users.noreply.github.com>
2023-10-13 21:39:42 -0500

    front end tienda electronica

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+
 @Module({
+  
   imports: [
     MongooseModule.forRoot('mongodb://root:secret@localhost:27017/?authMechanism=SCRAM-SHA-1', { dbName: 'tienda-electronica' }),
     ProductModule],
